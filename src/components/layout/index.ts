8ae5eb9c20c8af5ec0c336f4b11e890942dc4e58
Felix Erdmann <hello@felixerdmann.com>
2025-11-18 19:01:32 +0100

    fix: simplify shadcn ui element usage

--- src/components/layout/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Layout components exports
- */
-
-export { CardHeader } from "./CardHeader";
-export { CategoryTabs } from "./CategoryTabs";
-export { ColorSelector } from "./ColorSelector";
-export { PartsGrid } from "./PartsGrid";
-export { ActionButtons } from "./ActionButtons";
