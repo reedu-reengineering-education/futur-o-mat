068ef01979a40ebaa4d83cc12b0fcb5188228204
Felix Erdmann <hello@felixerdmann.com>
2025-11-25 21:52:34 +0100

    fix: update wimmelbild selection on mobile

--- src/components/wimmelbild/wimmelbild.tsx
+++ src/components/wimmelbild/wimmelbild.tsx
@@ -43,12 +43,10 @@ const Wimmelbild: React.FC<WimmelbildProps> = ({ images }) => {
             <div className="grid gap-4">
               <CardHeader>
                 <CardTitle>{image.title}</CardTitle>
-                <CardDescription className="min-h-32">
-                  {image.description}
-                </CardDescription>
+                <CardDescription>{image.description}</CardDescription>
               </CardHeader>
 
-              <div className="w-60 h-auto mx-auto rounded-xl overflow-hidden ring-3 ring-primary ring-offset-2">
+              <div className="w-full h-auto mx-auto rounded-xl overflow-hidden ring-3 ring-primary ring-offset-2">
                 <img
                   src={image.source}
                   alt={image.title}
@@ -59,32 +57,34 @@ const Wimmelbild: React.FC<WimmelbildProps> = ({ images }) => {
           )}
 
           {/* Untere Vorschau */}
-          <div className="bg-gray-100 rounded-xl p-6 mt-4">
+          <div className="bg-gray-100 rounded-xl py-4 px-2 mt-8 max-w-full">
             <p className="text-sm text-gray-600 mb-4 text-center font-medium">
               Verfügbare Wimmelbilder:
             </p>
-            <ScrollArea className="flex overflow-hidden">
-              <div className="flex w-max space-x-4 p-4 mb-2">
-                {images.map((wb_image) => (
-                  <button
-                    key={wb_image.id}
-                    className={`shrink-0 w-20 aspect-square rounded-xl overflow-hidden border-2 transition-all ${
-                      image?.id === wb_image.id
-                        ? "border-primary ring-4 ring-primary/20 scale-110"
-                        : "border-gray-300 hover:border-primary hover:scale-105"
-                    }`}
-                    onClick={() => handleImageSelect(wb_image)}
-                  >
-                    <img
-                      src={wb_image.source}
-                      alt={wb_image.title}
-                      className="w-full h-full object-cover"
-                    />
-                  </button>
-                ))}
-              </div>
-              <ScrollBar orientation="horizontal" />
-            </ScrollArea>
+            <div className="flex">
+              <ScrollArea className="w-1 flex-1">
+                <div className="flex w-max space-x-4 py-4 px-2">
+                  {images.map((wb_image) => (
+                    <button
+                      key={wb_image.id}
+                      className={`h-20 aspect-square rounded-xl overflow-hidden border-2 transition-all ${
+                        image?.id === wb_image.id
+                          ? "border-primary ring-4 ring-primary/20"
+                          : "border-gray-300 hover:border-primary"
+                      }`}
+                      onClick={() => handleImageSelect(wb_image)}
+                    >
+                      <img
+                        src={wb_image.source}
+                        alt={wb_image.title}
+                        className="w-full h-full object-cover"
+                      />
+                    </button>
+                  ))}
+                </div>
+                <ScrollBar orientation="horizontal" />
+              </ScrollArea>
+            </div>
           </div>
 
           <CardFooter className="flex justify-end mt-4">
