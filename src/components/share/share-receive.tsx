d974f391a68aba62af25fef2b49f572c7e6407a2
Felix Erdmann <hello@felixerdmann.com>
2025-11-28 09:33:14 +0100

    feat: update share receive page

--- src/components/share/share-receive.tsx
+++ src/components/share/share-receive.tsx
@@ -12,8 +12,16 @@ import {
 import AvatarCanvas from "../avatar/AvatarCanvas";
 import { Button } from "../ui/button";
 import { UserPlus } from "lucide-react";
+import { useState } from "react";
+import { STRENGTH_INFO, VALUE_INFO } from "@/assets/strengths-values.json";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+import Arrow3 from "../icons/arrow-swirl-up";
+import Arrow4 from "../icons/arrow-up-right";
 
 export default function ShareReceive() {
+  const [isStrengthDialogOpen, setIsStrengthDialogOpen] = useState(false);
+  const [isValueDialogOpen, setIsValueDialogOpen] = useState(false);
+
   const { shareState } = useParams({ strict: false });
   const { decodeState } = useShare();
   const decoded = decodeState(shareState);
@@ -28,44 +36,90 @@ export default function ShareReceive() {
     );
   }
 
-  const shareParams: ShareParams = decoded;
+  const {
+    avatar,
+    result: { strengthKey, valueKey },
+    wimmelbild,
+  }: ShareParams = decoded;
+
+  const strengthInfo = STRENGTH_INFO[strengthKey as keyof typeof STRENGTH_INFO];
+  const valueInfo = VALUE_INFO[valueKey as keyof typeof VALUE_INFO];
 
   return (
     <Layout>
+      <Dialog
+        open={isStrengthDialogOpen}
+        onOpenChange={setIsStrengthDialogOpen}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{strengthInfo?.title}</DialogTitle>
+          </DialogHeader>
+          {strengthInfo?.description}
+        </DialogContent>
+      </Dialog>
+
+      <Dialog open={isValueDialogOpen} onOpenChange={setIsValueDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{valueInfo?.title}</DialogTitle>
+          </DialogHeader>
+          {valueInfo?.description}
+        </DialogContent>
+      </Dialog>
       <Card className="max-w-md">
-        <CardContent className="grid gap-6">
-          <CardHeader>
-            <CardTitle>Futur-O-Mat</CardTitle>
-            <CardDescription>
-              Mach dir die Zukunft, wie sie dir gefällt!
-            </CardDescription>
-          </CardHeader>
-          <div className="relative max-w-64 aspect-square rounded-2xl overflow-hidden ring-3 ring-primary ring-offset-2 flex items-center justify-center mx-auto">
-            {shareParams.wimmelbild && (
-              <img
-                src={shareParams.wimmelbild.source}
-                alt="Wimmelbild Hintergrund"
-                className="w-full h-full object-cover inset-0 opacity-90"
-              />
-            )}
-            {shareParams.avatar && (
+        <CardHeader>
+          <CardTitle>Futur-O-Mat</CardTitle>
+          <CardDescription>
+            Mach dir die Zukunft, wie sie dir gefällt!
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-12">
+          <div className="flex flex-col">
+            <p className="font-semibold ">Wert</p>
+            <p className="bg-primary rounded px-2 py-1 font-medium text-primary-foreground text-sm w-fit">
+              {valueInfo?.title}
+            </p>
+          </div>
+          {/* Avatar Bild */}
+          <div className="flex justify-center relative">
+            <div className="relative w-full aspect-square rounded-2xl overflow-hidden ring-3 ring-primary ring-offset-2 flex items-center justify-center mx-auto">
+              {wimmelbild && (
+                <img
+                  src={wimmelbild.source}
+                  alt="Wimmelbild Hintergrund"
+                  className="w-full h-full object-cover inset-0 opacity-30"
+                />
+              )}
               <AvatarCanvas
-                avatarConfig={shareParams.avatar}
-                quizResult={shareParams.result}
-                className="h-full absolute"
+                avatarConfig={avatar}
+                quizResult={{ strengthKey, valueKey }}
                 showStrengh
                 showValue
+                onStrengthClick={() => setIsStrengthDialogOpen(true)}
+                onValueClick={() => setIsValueDialogOpen(true)}
+                className="h-full absolute"
               />
-            )}
+            </div>
+            <Arrow4 className="absolute -top-6 -left-4 h-12 rotate-25 text-primary scale-y-[-1]" />
+
+            <Arrow3 className="absolute bottom-0 right-0 h-20 text-primary -rotate-55" />
           </div>
-          <CardFooter>
-            <Link to={"/"} className="w-full">
-              <Button className="w-full">
-                Erstelle deinen eigenen Avatar <UserPlus />
-              </Button>
-            </Link>
-          </CardFooter>
+
+          <div className="flex flex-col items-end">
+            <p className="bg-primary rounded px-2 py-1 font-medium text-primary-foreground text-sm w-fit">
+              {strengthInfo?.title}
+            </p>
+            <p className="font-semibold">Stärke</p>
+          </div>
         </CardContent>
+        <CardFooter>
+          <Link to={"/"} className="w-full">
+            <Button className="w-full">
+              Erstelle deinen eigenen Avatar <UserPlus />
+            </Button>
+          </Link>
+        </CardFooter>
       </Card>
     </Layout>
   );
