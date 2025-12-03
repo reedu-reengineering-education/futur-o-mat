e47f9458243d5afbee3dd7fb5e079ebf7aa2c3dd
Felix Erdmann <hello@felixerdmann.com>
2025-12-03 14:51:37 +0100

    fix: remove preload image on main

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { ErrorBoundary } from "./components/ErrorBoundary";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 // Import the auto generated route tree
 import { routeTree } from "./routeTree.gen";
-import { preloadAvatarImages } from "./utils/preloadImages";
 
 import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/500.css";
@@ -16,9 +15,6 @@ import "./index.css";
 
 // Create a new router instance
 const router = createRouter({ routeTree, basepath: "/" });
-
-// Start preloading avatar images in the background
-preloadAvatarImages();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
